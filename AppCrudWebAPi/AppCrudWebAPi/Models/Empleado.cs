18f16782bbfbd7a6aabfc44b028eebf6f6150509
Jesus <jesus.test09@gmail.com>
2020-06-22 03:56:01 -0500

    Implementacion de entidades empleado

---
+++ AppCrudWebAPi/AppCrudWebAPi/Models/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppCrudWebAPi.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public DateTime FechaContratación { get; set; }
+        public string Dirección { get; set; }
+        public string Ciudad { get; set; }
+        public string Pais { get; set; }
+        public string TelDomicilio { get; set; }
+
+    }
+}
---
+++ AppCrudWebAPi/AppCrudWebAPi/Models/Reply.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppCrudWebAPi.Models
+{
+    public class Reply
+    {
+        public int Result { get; set; }
+        public object data { get; set; }
+        public string message { get; set; }
+
+    }
+}
